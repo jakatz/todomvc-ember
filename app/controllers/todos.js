f73cde2c732bda6adc7ff6e4d132396aee3125d5
Jonathan Katz <jonathan.a.katz@gmail.com>
2015-03-23 17:03:17 -0700

    able to check all items as completed

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -26,6 +26,16 @@ export default Ember.ArrayController.extend({
 		}
 	},
 
+	allAreDone: function(key, value) {
+		if (value === undefined) {
+			return !!this.get('length') && this.isEvery('isCompleted')
+		} else {
+			this.setEach('isCompleted', value);
+			this.invoke('save');
+			return value;
+		}
+	}.property('@each.isCompleted'),
+
 	hasCompleted: function() {
 		return this.get('completed') > 0;
 	}.property('completed'),
